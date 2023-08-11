99b886ae705314d443f8e0ddb3c1472477096e59
Muhammad Ilham El Hakim <m.ilhamhakim@gmail.com>
2023-08-11 09:04:31 +0700

    fixing bottom navigation bar

--- lib/shared/customs/bottom_navigation_custom.dart
+++ lib/shared/customs/bottom_navigation_custom.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:trainee/configs/themes/main_color.dart';
 import 'package:trainee/modules/global_controllers/navigation_controller.dart';
+import 'package:trainee/shared/styles/google_text_style.dart';
 
 class BottomNavigation extends StatelessWidget implements PreferredSizeWidget {
   const BottomNavigation({super.key});
@@ -31,86 +32,102 @@ class BottomNavigation extends StatelessWidget implements PreferredSizeWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                color: MainColor.white,
-                onPressed: () {
-                  NavigationController.to.tabIndex(0);
-                },
-                icon: Obx(() => Icon(
-                      Icons.home,
-                      color: NavigationController.to.tabIndex.value == 0  
-                          ? Colors.white
-                          : const Color.fromARGB(105, 255, 255, 255),
-                      size: 30,
-                    )),
-              ),
-              Text(
-                "Home".tr,
-                style: TextStyle(
+          
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
                   color: MainColor.white,
-                  fontSize: 12.sp,
+                  onPressed: () {
+                    NavigationController.to.tabIndex(0);
+                  },
+                  icon: Obx(() => Icon(
+                        Icons.home,
+                        color: NavigationController.to.tabIndex.value == 0  
+                            ? Colors.white
+                            : const Color.fromARGB(105, 255, 255, 255),
+                        size: 30,
+                      )),
                 ),
-              ),
-            ],
+                Flexible(
+                  child: Text(
+                    "Home".tr,
+                    style: GoogleTextStyle.fw400.copyWith(
+                      color: MainColor.white,
+                      fontSize: 12.sp,
+                    ),
+                     textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
           ),
-          Column(
-             mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                color: MainColor.white,
-                onPressed: () {
-                  NavigationController.to.tabIndex(1);
-                },
-                icon: Obx(() => Icon(
-                      Icons.food_bank_rounded,
-                      size: 30,
-                      color: NavigationController.to.tabIndex.value == 1
-                          ? MainColor.white
-                          : const Color.fromARGB(105, 255, 255, 255),
-                    )),
-              ),
-              Text(
-                "Order".tr,
-                style: TextStyle(
+
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
                   color: MainColor.white,
-                  fontSize: 12.sp,
+                  onPressed: () {
+                    NavigationController.to.tabIndex(1);
+                  },
+                  icon: Obx(() => Icon(
+                        Icons.food_bank_rounded,
+                        size: 30,
+                        color: NavigationController.to.tabIndex.value == 1
+                            ? MainColor.white
+                            : const Color.fromARGB(105, 255, 255, 255),
+                      )),
                 ),
-              ),
-            ],
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                   NavigationController.to.tabIndex(2);
-                },
-                icon: Obx(() => Icon(
-                      Icons.account_circle_outlined,
-                      size: 30,
-                      color: NavigationController.to.tabIndex.value == 2
-                          ? MainColor.white
-                          : const Color.fromARGB(105, 255, 255, 255),
-                    )),
-              ),
-              Text(
-                "Profile".tr,
-                style: TextStyle(
-                  color: MainColor.white,
-                  fontSize: 12.sp,
+                Flexible(
+                  child: Text(
+                    "Order".tr,
+                    style: GoogleTextStyle.fw400.copyWith(
+                      color: MainColor.white,
+                      fontSize: 12.sp,
+                    ),
+                     textAlign: TextAlign.center,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                     NavigationController.to.tabIndex(2);
+                  },
+                  icon: Obx(() => Icon(
+                        Icons.account_circle_outlined,
+                        size: 30,
+                        color: NavigationController.to.tabIndex.value == 2
+                            ? MainColor.white
+                            : const Color.fromARGB(105, 255, 255, 255),
+                      )),
+                ),
+                Flexible(
+                  child: Text(
+                    "Profile".tr,
+                    style: GoogleTextStyle.fw400.copyWith(
+                      color: MainColor.white,
+                      fontSize: 12.sp,
+                    ),
+                     textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
         ],
       ),
     );
